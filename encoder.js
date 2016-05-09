3d382e6076f327dd62145b933b4fdc52c3e6e674
lingmm <450044101@qq.com>
2016-05-09 14:56:34 +0800

    fixed

--- encoder.js
+++ encoder.js
@@ -2,13 +2,12 @@ const config = require('./config');
 
 const chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
 	'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const count = chars.length + 1;
 
 const num2chars = (num, min = config.minimum) => {
 	let tmp = '';
 	do {
-		tmp = chars[num % count] + tmp;
-		num = Math.floor(num / count);
+		tmp = chars[num % chars.length] + tmp;
+		num = Math.floor(num / chars.length);
 	} while (num > 0);
 	return tmp.length > min ? tmp : chars[0].repeat((min - tmp.length)) + tmp;
 }
@@ -18,8 +17,7 @@ const chars2num = (str) => {
 	let end = 0;
 	let num = 0;
 	do {
-		let a = (Math.pow(count, end++));
-		num += a * chars.indexOf(chrs.pop());
+		num += Math.pow(chars.length, end++) * chars.indexOf(chrs.pop());
 	} while (chrs.length > 0);
 	return num;
 }
