0156249309144a46c0e2b48c488c5bbf336d7159
shyling <450044101@qq.com>
2016-05-22 01:15:08 +0800

    update i18n
    如果对应语言的翻译不存在时将会显示默认语言的翻译 or key name

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,13 +1,5 @@
 const language = require('./values');
 
-const Gen = (locale) => {
-	return new Proxy({}, {
-		get: function (target, name) {
-			return language[name] && language[name][locale] || name;
-		}
-	});
-}
-
 let supported = [];
 
 Reflect.ownKeys(language).forEach((i) => {
@@ -18,6 +10,15 @@ Reflect.ownKeys(language).forEach((i) => {
 	}
 });
 
+const Gen = (locale) => {
+	return new Proxy({}, {
+		get: function (target, name) {
+			return language[name] && language[name][locale] || language[name][supported[0]] || name;
+		}
+	});
+}
+
+//choose best locale for render
 let i18n = (req, res, next) => {
 	req.locale = (req.acceptsLanguages() || []).find(_ => supported.indexOf(_) !== -1) || supported[0];
 	res._ = res.locals._ = Gen(req.locale);
