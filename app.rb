4840f6987ff2c59bbc0428c95d08266eda2ff6bf
lingmm <450044101@qq.com>
2016-06-05 18:26:32 +0800

    add redis config

--- app.rb
+++ app.rb
@@ -4,13 +4,8 @@ require 'yaml'
 
 module HashHelper
 	CHARS = %w(
-		0 1 2 3 4 5 6 7 8 9
-		A B C D E F G H I J
-		K L M N O P Q R S T
-		U V W X Y Z a b c d
-		e f g h i j k l m n
-		o p q r s t u v w x
-		y z
+		0 1 2 3 4 5 6 7 8 9	A B C D E F G H I J	K L M N O P Q R S T	U
+		V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z
 	)
 	def encode(i)
 		tmp = ''
@@ -25,14 +20,23 @@ module HashHelper
 	end
 end
 
+module Config
+	def self.all
+		@all = YAML.load_file './config.yaml' if @all.nil?
+		@all
+	end
+	def self.method_missing(name,*args,&block)
+		self.all[name.id2name]
+	end
+end
+
 module DBHelper
 	include HashHelper
 	
-	@@connection = Redis.new
+	@@connection = Redis.new Config.redis_config
 	@@counter = :'~COUNT'
 	def put_url(url)
-		i = @@connection.incr @@counter
-		s = encode(i)
+		s = encode(@@connection.incr @@counter)
 		@@connection.set s,url
 		s
 	end
@@ -46,12 +50,6 @@ end
 class App < Sinatra::Base
 	include DBHelper
 	
-	def prefix
-		return @prefix unless @prefix.nil?
-		@prefix = YAML.load(File.open('./config.yaml'))['config']['prefix']
-		@prefix
-	end
-	
 	configure do
 		set :public_folder, File.dirname(__FILE__) + '/public'
 		disable :show_exceptions
@@ -63,7 +61,7 @@ class App < Sinatra::Base
 	
 	get '/:hash' do |hash|
 		begin
-			url = get_url(hash)
+			url = get_url hash
 			if url
 				redirect url,302
 			else
@@ -77,9 +75,9 @@ class App < Sinatra::Base
 	post '/' do
 		url = params[:url].to_s
 		begin
-			raise :网址过长 if url.length > 8192
-			raise :网址格式错误 unless /^\w+:\/\/.+/ =~ url
-			erb :index, locals: { result: prefix + put_url(url) }
+			raise '网址过长' if url.length > 8192
+			raise '网址格式错误' unless /^\w+:\/\/.+/ =~ url
+			erb :index, locals: { result: Config.url_prefix + put_url(url) }
 		rescue => e
 			erb :error, locals: { error: e.message }
 		end
